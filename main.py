d6a947227420f616f27071c43d9859f46d726d84
Bedo Kharboutli <kharboutli@luday.se>
2024-11-13 14:36:38 +0100

    Converting-time function to make time readable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def get_channels():
@@ -21,21 +21,28 @@ def get_schedule(channel_id):
     
     return episodes
 
+# omvandla UNIX-tidsstämpeln från API till ett läsbart format
+def convert_time(time_string):
+    
+    timestamp = int(time_string[6:-2]) // 1000
+    readable_time = datetime.fromtimestamp(timestamp, tz=timezone.utc).strftime('%H:%M')
+    return readable_time
+
 def main():
     channels = get_channels()
     
     print("Menu – Choose a radio station:")
     for i, (channel_id, channel_name) in enumerate(channels, 1):
         print(f"{i}. {channel_name}")
-    
+
     choice = int(input("\n### Choice? "))
     if 1 <= choice <= len(channels):
         chosen_channel_id, chosen_channel_name = channels[choice - 1]
         print(f"\nShowing the next 10 programs for {chosen_channel_name}:\n")
-        
+
         episodes = get_schedule(chosen_channel_id)
         for episode in episodes:
-            start_time = episode['starttimeutc']
+            start_time = convert_time(episode['starttimeutc'])
             title = episode['title']
             print(f"* {start_time} – {title}")
     else:
